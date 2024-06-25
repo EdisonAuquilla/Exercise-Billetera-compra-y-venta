2f8c0a8374d2bcf52e689618ca1f0641c990cba5
EdisonAuquilla <edisonauquilla.25@gmail.com>
2024-06-25 00:15:50 -0500

    firts commit

---
+++ src/main/java/com/mycompany/billetera/DefualtTableModel.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.billetera;
+
+/**
+ *
+ * @author Leght Moon69
+ */
+class DefualtTableModel {
+int casRest= 100;
+
+    void setCulumIdentifiers(String[] Title) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void addRow() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void addRow(Object[] object) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
